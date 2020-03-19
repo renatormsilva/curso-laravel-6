2b56c5f1692aa5918bd6a3b742162042373370ce
renatodln123 <renato.brtkm@gmail.com>
2020-03-19 16:33:45 -0400

    Grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,40 @@
 |
 */
 
+Route::get('/login', function(){
+    return 'login';
+})->name('login');
+
+Route::middleware(['auth'])->group(function(){
+
+    Route::prefix('admin')->group(function(){
+        Route::get('/dashboard', function(){
+            return 'Home admin';
+        });
+    
+        Route::get('/financeiro', function(){
+            return 'Financeiro admin';
+        });
+    
+        Route::get('/produtos', function(){
+            return 'Produtos admin';
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+Route::get('/registro/{flag?}', function($flag = '') {
+    return "Seu nome é {$flag}";
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
