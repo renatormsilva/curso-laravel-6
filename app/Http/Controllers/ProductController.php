f50f08cb3c3bcff17044c0c475b83d183350bc47
renatodln123 <renato.brtkm@gmail.com>
2020-03-22 01:46:40 -0400

    middlewares

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,16 @@ class ProductController extends Controller
 
     public function __construct(Request $request)
     {
-        $this->request = $request;
+       /* $this->request = $request;
+        //$this->middleware('auth');
+        $this->middleware('auth')->only([
+            'create','store'
+
+        ]); */
+        $this->middleware('auth')->except([
+            'index','show'
+        ]);
+
     }
 
 
